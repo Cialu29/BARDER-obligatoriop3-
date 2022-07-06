85bfcf076eea14c45a3133ef30b6b39d070ae084
yohana <74024203+yohanamaidana25@users.noreply.github.com>
2022-07-05 22:15:02 -0300

    creación de rama decelop

--- DataAccess/DataBase/UsuarioBardo.cs
+++ DataAccess/DataBase/UsuarioBardo.cs
@@ -1,11 +1,13 @@
+
+
 namespace DataAccess.DataBase
 {
+
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-
     [Table("UsuarioBardo")]
     public partial class UsuarioBardo
     {
